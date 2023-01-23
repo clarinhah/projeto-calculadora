cb4ba62402146caa6f5897843d57607e19fe25c5
Clarice Monteiro <115654741+clarinhah@users.noreply.github.com>
2023-01-23 00:02:12 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+const resultado = document.querySelector(".result");
+const confirmar = document.querySelector(".igual");
+
+
+function insert(valor){
+    resultado.innerHTML += valor; 
+}
+
+function clean(){
+    resultado.innerHTML = '';
+}
+
+
+function backspace(){
+    if(resultado.textContent){
+        let result = document.querySelector('#resultado').innerHTML
+        resultado.innerHTML = result.substring(0, result.length -1);
+    }
+    
+}
+
+
+
+function confirm(){
+    if(resultado.textContent != 'Erro'){
+        document.getElementById('resultado').innerHTML = eval(resultado.innerHTML)
+    }
+}
